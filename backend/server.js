ae704053db81bb5b8c0d03b238460e68211effc4
syed-shaida-hussain <85978159+syed-shaida-hussain@users.noreply.github.com>
2023-07-31 22:50:17 +0530

    Created test port

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 const express = require("express")
+require("dotenv").config()
 
 
 const app = express();
 
+app.get("/" , (req,res) => {
+    res.status(200).send("success")
+})
 
-app.listen(5051, () => {
-    console.log(`App listened to ${5051}`)
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+    console.log(`App listened to ${PORT}`)
 })
